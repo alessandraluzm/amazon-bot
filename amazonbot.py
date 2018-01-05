63ef93e4dcbd400ac48cbad4da4e627302a562fa
alessandraluzm <alessandraluzm@ulbra.edu.br>
2018-01-04 23:50:58 -0200

    Armazenando valor em texto e em número para poder utilizar na comparação dos preços

--- amazonbot.py
+++ amazonbot.py
@@ -21,12 +21,18 @@ def main():
     # buscar o nome da lista de desejos
     print(soup.find('span', id='profile-list-name').text)
 
-    dicionario = {}
+    dicionario = {} 
     for div in soup.find_all('div', class_="a-text-left a-fixed-left-grid-col g-item-sortable-padding a-col-right"):
         titulo = div.find("a", class_="a-link-normal a-declarative").text
         valor = div.find("span", class_="a-offscreen").text
-        dicionario[titulo] = valor
-
+        novo_valor = valor.split('R$ ')[1]
+        novo_valor = novo_valor.replace(',', '.')
+        novo_valor = float(novo_valor)
+        print(novo_valor)
+        dicionario[titulo] = {}
+        dicionario[titulo]={'string': valor}
+        dicionario[titulo]['inteiro']= novo_valor        
+        
     print(dicionario)
 
     with open('dicionario.json','r', encoding='utf8') as f:
@@ -35,25 +41,21 @@ def main():
         dicionario_anterior = json.loads(leitura)
         
         for key, value in dicionario.items():
-            if dicionario[key] < dicionario_anterior[key]:
-                print('Value: {}'.format(value))
+            if key in dicionario_anterior:
+                if dicionario[key]['inteiro'] < dicionario_anterior[key]['inteiro']:
+                    print('Value: {}'.format(dicionario[key]['inteiro']))
     # carregar o dicionario antigo do arquivo
     # iterar sobre os itens do dicionario antigo e comparar com o atual os preços
     # se o preço atual for menor que o antigo, printar o livro
 
-
-    with open('dicionario.json', 'w', encoding='utf8') as f:
-        tarefa = f.write("mozao lindao")
-        f.write("\nrick and morty rlz")
-
     with open('dicionario.json', 'r', encoding='utf8') as f:
         a = f.read()
-        print(a)
+        #print(a)
 
         import json
         s = json.dumps(dicionario, indent=2)
-        with open('dicionario.json', 'w') as f:
-            f.write(s)
+        #with open('dicionario.json', 'w') as f:
+          #  f.write(s)
 
 def leitura():
     with open('dicionario.json', 'r') as f:
